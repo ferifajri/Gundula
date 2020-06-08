c9c6dab7c154f14c79ad2d4d559fe0b929e8f824
Feri Fajri <feri.okana@gmail.com>
2020-06-08 08:59:01 +0700

    Initial Commit

---
+++ Gundula/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gundula
+//
+//  Created by Feri Fajri on 08/06/20.
+//  Copyright © 2020 Feri Fajri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
